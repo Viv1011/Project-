78892548f24f8bb2a366c2199c0d243f224c5fa5
Viv1011 <118555455+Viv1011@users.noreply.github.com>
2023-10-21 11:26:34 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+music1="";
+music2="";
+
+function preload(){
+    music1 = loadSound('Song1.mp3');
+    music2 = loadSound('Song2.mp3');
+}
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    vi
+}
